8691682c9c9e9210d50baf708c7b863075ef6141
Michael Allen <mike@mistColoured.com>
2017-12-08 13:18:46 +0000

    Tryng to work out which project this is

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input onChange={(event) => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+                {/*Value of the input: { this.state.term }*/}
+            </div>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBM1Kq8qgmoLFV0mqJ2JpnKxSkC-bAjcWc';
 
-const API_KEY = 'AIzaSyAf6qUOWqmdEHyUrFTsCF_Z0zrlT7N1MYI';
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => { this.setState ({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 };
-// Take this component's generated HTML and put it on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+
